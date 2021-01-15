14537a45b04fd405596708a8b2bf30de5a2b5af8
robertgizelo <robertgizelo25@gmail.com>
2021-01-15 14:37:12 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import TodoList from './Todo/TodoList'
+import AddTodo from './Todo/AddTodo'
+import Context from './context'
+import Loading from './Loading'
+import Modal from './Modal/Modal'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                    setLoading(false)
+                }, 0)
+            })
+    }, [])
+
+    function toggleTodo(id) {
+        setTodos(
+            todos.map(todo => {
+                if (todo.id === id) {
+                    todo.done = !todo.done
+                }
+                return todo
+            })
+        )
+    }
+
+    function addTodo(title) {
+        setTodos(
+            todos.concat([
+                {
+                    title,
+                    id: Date.now(),
+                    done: false
+                }
+            ])
+        )
+    }
+
+    function removeTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    function removeAllTodos(event) {
+        event.preventDefault();
+        setTodos([])
+    }
+
+    return (
+        <Context.Provider value={{removeTodo}}>
+            <div className="wrapper">
+                <div style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                    <h1 className="app-header">TodoApp</h1>
+                    <Modal/>
+                </div>
+
+                <AddTodo onCreate={addTodo} rmAllTds={removeAllTodos}/>
+                {loading && <Loading/>}
+                {todos.length ? (
+                        <TodoList todos={todos} onToggle={toggleTodo}/>)
+                    : (
+                        loading ? null : (<p style={{color: "white"}}>You've got no todos</p>)
+                    )}
+            </div>
+        </Context.Provider>
+    );
 }
 
 export default App;
